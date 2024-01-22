65507c26840f98b43854adf4456eccbe8c86cd54
ColoradaGreis <graciana.baratti@gmail.com>
2024-01-22 17:43:16 -0300

    [feat] add new props

--- convertir-csv-a-json.js
+++ convertir-csv-a-json.js
@@ -1,21 +1,22 @@
 const csvtojson = require('csvtojson');
 const fs = require('fs');
 
-const csvFilePath = 'tu_archivo.csv';
+const csvFilePath = 'productsCSV.csv';
 const jsonFilePath = 'resultado.json';
 
 csvtojson({ delimiter: ';' })
   .fromFile(csvFilePath)
   .then((jsonArray) => {
     const jsonData = jsonArray.map((row) => {
-      // Convierte la primera letra a mayúsculas y el resto a minúsculas
-      const formattedLongName = row["long_name"].toLowerCase().replace(/\b\w/g, l => l.toUpperCase());
-      const id = parseInt(row["id"], 10)
+      const formattedLongName = row["long_name"].toLowerCase().replace(/\b\w/g, l => l.toUpperCase()) || 'No Encontrado';
       const contentDetail = parseFloat(row["content_detail"])
+      const generated_ean = row["generated_ean"] || false
+      const pack = [row["pack"]] || false
 
       return {
-        "id": id,
         "ean": row["ean"],
+        "sku": row["sku"],
+        "sap": row["sap"],
         "short_name": row["short_name"],
         "long_name": formattedLongName,
         "content": {
@@ -23,16 +24,14 @@ csvtojson({ delimiter: ';' })
           "measure_unit": row["measure_unit"]
         },
         "brand": row["brand"],
-        "first_category": row["first_category"],
-        "second_category": row["second_category"],
+        "category": row["first_category"],
         "proxy_duration": row["proxy_duration"],
-        "is_pack": row["is_pack"],
-        "primary_image": row["primary_image"],
+        "pack": pack,
         "aisle": row["aisle"],
+        "generated_ean": generated_ean,
+        "autoshoppable_available": true,
       };
     });
-
-    // Guardar el resultado en un archivo JSON
     fs.writeFileSync(jsonFilePath, JSON.stringify(jsonData, null, 2), 'utf-8');
 
     console.log('Conversión completa.');
